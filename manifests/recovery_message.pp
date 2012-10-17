048fd95062fe7be9f300e45138b776214b0f9d0e
Mike Skalnik <mike.skalnik@gmail.com>
2012-10-17 14:52:45 -0500

    Add osx_recovery_message type

---
+++ manifests/recovery_message.pp
@@ -0,0 +1,26 @@
+define osx_recovery_message(
+  $ensure = 'present',
+  $domain = undef,
+  $key    = undef,
+  $value  = undef,
+  $user   = undef,
+) {
+  if $ensure == 'present' {
+    if $value != undef {
+      property_list_key { 'Set OS X Recovery Message':
+        ensure => present,
+        path   => '/Library/Preferences/com.apple.loginwindow.plist',
+        key    => 'LoginwindowText',
+        value  => $value
+      }
+    } else {
+      warning('Cannot set an OS X recovery message without a value')
+    }
+  } else {
+    property_list_key { 'Remove OS X Recovery Message'
+      ensure => absent,
+      path   => '/Library/Preferences/com.apple.loginwindow.plist',
+      key    => 'LoginwindowText'
+    }
+  }
+}
