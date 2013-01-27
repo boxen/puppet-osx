7e128d28b5be420fc2da8cd63bae526166ae64d8
Tim Sharpe <tim@sharpe.id.au>
2013-01-27 14:35:23 -0800

    Remove some cruft

--- Puppetfile
+++
@@ -1,1 +0,0 @@
-mod "property_list_key", "0.1.0",  :github_tarball => "glarizza/puppet-property_list_key"
--- spec/fixtures/modules/property_list_key/tests/absent.pp
+++
@@ -1,5 +0,0 @@
-property_list_key { 'simple':
-  ensure => absent,
-  key    => 'simple',
-  path => '/tmp/com.puppetlabs.puppet',
-}
