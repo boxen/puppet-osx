206c8026c32e215b619e8d10a411cc668695e28f
tekkub :bear:  <tekkub@gmail.com>
2013-03-05 17:21:07 -0700

    Only unhide ~/Library if it's hidden

--- manifests/finder/unhide_library.pp
+++ manifests/finder/unhide_library.pp
@@ -3,6 +3,7 @@ class osx::finder::unhide_library {
   $home = "/Users/$::boxen_user"
 
   exec { 'Unhide ~/Library':
-    command => "chflags nohidden $home/Library";
+    command => "chflags nohidden $home/Library",
+    onlyif  => "ls -Ol $home | grep Library | grep hidden";
   }
 }
