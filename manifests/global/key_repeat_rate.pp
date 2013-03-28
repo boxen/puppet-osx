d0443e6329e3b6fdf30c216a80bc4e9184374708
Ben Bleything <ben@bleything.net>
2013-03-28 17:28:41 -0400

    convert key_repeat_rate to a class and add docs

--- manifests/global/key_repeat_rate.pp
+++ manifests/global/key_repeat_rate.pp
@@ -1,7 +1,18 @@
-
-define osx::global::key_repeat_rate($rate = 0) {
+# Public: Sets the repeat rate for held keys.
+#
+# rate - the delay between key 'presses'. Defaults to 0.
+#
+# Examples
+#
+#   # set the repeat rate to 0 (the default)...
+#   include osx::global::key_repeat_rate
+#
+#   # ...or pick your own repeat rate!
+#   class { 'osx::global::key_repeat_rate':
+#     rate => 2
+#   }
+class osx::global::key_repeat_rate($rate = 0) {
   boxen::osx_defaults { 'key repeat rate':
-    ensure => present,
     domain => 'NSGlobalDomain',
     key    => 'KeyRepeat',
     value  => $rate,
