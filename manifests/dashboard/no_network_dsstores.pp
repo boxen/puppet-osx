c56fcd767317ce64768d2bec017b08bdc36eda4d
Ben Bleything <ben@bleything.net>
2013-03-28 17:38:50 -0400

    add docs to existing classes

--- manifests/dashboard/no_network_dsstores.pp
+++ manifests/dashboard/no_network_dsstores.pp
@@ -1,4 +1,4 @@
-
+# Public: Disable creation of .DS_Store files on network drives.
 class osx::dashboard::no_network_dsstores {
   boxen::osx_defaults { 'Do not create .DS_Store on network shares':
     key    => 'DSDontWriteNetworkStores',
--- manifests/disable_app_quarantine.pp
+++ manifests/disable_app_quarantine.pp
@@ -1,4 +1,4 @@
-
+# Public: Disable the App Quarantine feature so you can run apps you download.
 class osx::disable_app_quarantine {
   boxen::osx_defaults { 'Disable the downloaded app quarantine':
     user   => $::boxen_user,
