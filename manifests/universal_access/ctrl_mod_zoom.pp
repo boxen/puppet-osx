1673e2e56dab736e2f5f454e0d3184d1a6ca70c0
Ben Bleything <ben@bleything.net>
2013-03-28 17:21:55 -0400

    document existing universal classes

--- manifests/universal_access/ctrl_mod_zoom.pp
+++ manifests/universal_access/ctrl_mod_zoom.pp
@@ -1,4 +1,4 @@
-
+# Public: Enables Ctrl-scroll to zoom.
 class osx::universal_access::ctrl_mod_zoom {
   boxen::osx_defaults { 'Use scroll gesture with the Ctrl (^) key to zoom':
     user   => $::boxen_user,
--- manifests/universal_access/enable_scrollwheel_zoom.pp
+++ manifests/universal_access/enable_scrollwheel_zoom.pp
@@ -1,4 +1,4 @@
-
+# Public: Enable use of mousewheel for zooming.
 class osx::universal_access::enable_scrollwheel_zoom {
   boxen::osx_defaults { 'Use mouse wheel (scroll gesture) to zoom':
     user   => $::boxen_user,
