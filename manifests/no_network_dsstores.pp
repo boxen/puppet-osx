d099df024889e8a0d73d9ac478ba9200026cb64f
Ben Bleything <ben@bleything.net>
2013-03-28 22:52:12 -0400

    Fix up no_network_dsstores class
    * move it to the top level
    * set the key in the correct domain
    * run as the boxen user

--- manifests/no_network_dsstores.pp
+++ manifests/no_network_dsstores.pp
@@ -1,8 +1,9 @@
 # Public: Disable creation of .DS_Store files on network drives.
-class osx::dashboard::no_network_dsstores {
+class osx::no_network_dsstores {
   boxen::osx_defaults { 'Do not create .DS_Store on network shares':
     key    => 'DSDontWriteNetworkStores',
-    domain => 'com.apple.dashboard',
-    value  => true;
+    domain => 'com.apple.desktopservices',
+    value  => true,
+    user   => $::boxen_user
   }
 }
