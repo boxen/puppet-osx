6548091c9647ee5f0a528befcccdf6b1c5f192a2
Ben Bleything <ben@bleything.net>
2013-04-17 19:26:51 -0700

    better description

--- manifests/global/disable_key_press_and_hold.pp
+++ manifests/global/disable_key_press_and_hold.pp
@@ -1,6 +1,6 @@
 # Public: Disables keyboard press-and-hold for accented character entry.
 class osx::global::disable_key_press_and_hold {
-  boxen::osx_defaults { 'fucking stupid popover':
+  boxen::osx_defaults { 'Disable press-and-hold for accented characters':
     ensure => present,
     domain => 'NSGlobalDomain',
     key    => 'ApplePressAndHoldEnabled',
--- spec/classes/global/disable_key_press_and_hold_spec.rb
+++ spec/classes/global/disable_key_press_and_hold_spec.rb
@@ -4,7 +4,7 @@ describe 'osx::global::disable_key_press_and_hold' do
   let(:facts) { {:boxen_user => 'ilikebees'} }
 
   it do
-    should contain_boxen__osx_defaults('fucking stupid popover').with({
+    should contain_boxen__osx_defaults('Disable press-and-hold for accented characters').with({
       :key    => 'ApplePressAndHoldEnabled',
       :domain => 'NSGlobalDomain',
       :value  => false,
