add726dadab6422f94747f6429a1ec62248ae1ca
John Britton <public@johndbritton.com>
2013-06-27 12:04:59 +0200

    autohide-delay is a float

--- manifests/dock/disable.pp
+++ manifests/dock/disable.pp
@@ -5,6 +5,7 @@ class osx::dock::disable {
   boxen::osx_defaults { 'Disable the dock':
     user   => $::boxen_user,
     key    => 'autohide-delay',
+    type   => 'float',
     domain => 'com.apple.dock',
     value  => 999999,
     notify => Exec['killall Dock'];
