ca47c2871041cbb5f9148112b0cc10b68c5c0a2d
John Britton <public@johndbritton.com>
2013-06-27 23:41:44 +0200

    add osx::software_update

---
+++ manifests/software_update.pp
@@ -0,0 +1,9 @@
+# Public: Install updates from Apple 
+class osx::software_update {
+  exec {
+    'OSX Software Update':
+      command => 'softwareupdate -i -a',
+      user    => 'root',
+      timeout => 0
+  }
+}
