cf0bc2257692d797d54bffce4a1412bfff2ba28b
Billy Vong <billyvg@gmail.com>
2013-11-20 23:53:48 -0800

    Add osx setting for Dock position

---
+++ manifests/dock/position.pp
@@ -0,0 +1,25 @@
+# Public: Sets the dock position
+#
+# position - 'bottom', 'top', 'right',left'
+#
+# Examples
+#
+#   # set the position to be on the bottom
+#   include osx::dock::position
+#
+#   # ...or pick your own position!
+#   class { 'osx::dock::position':
+#     position => 'left'
+#   }
+class osx::dock::position($position = 'right') {
+  include osx::dock
+
+  boxen::osx_defaults { 'orientation':
+    domain => 'com.apple.dock',
+    key    => 'orientation',
+    type   => 'string',
+    value  => $position,
+    user   => $::boxen_user,
+    notify => Exec['killall Dock'];
+  }
+}
