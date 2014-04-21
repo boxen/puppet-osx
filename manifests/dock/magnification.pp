ee909db4a601a06a3e5405a767a6377596cab063
W. S. Wellington <bill@wellingtonnet.net>
2014-04-20 21:11:13 -0700

    Getting the hang of what Travis is looking for
    whitespace gone...   quoted bool value gone
    
    Signed-off-by: W. S. Wellington <bill@wellingtonnet.net>

--- manifests/dock/magnification.pp
+++ manifests/dock/magnification.pp
@@ -17,7 +17,7 @@
 #
 
 
-class osx::dock::magnification ($magnification = 'true', $magnification_size = '128'){
+class osx::dock::magnification ($magnification = true, $magnification_size = '128'){
   include osx::dock
 
   boxen::osx_defaults { 'magnification':
@@ -28,7 +28,7 @@ class osx::dock::magnification ($magnification = 'true', $magnification_size = '
     user   => $::boxen_user,
     notify => Exec['killall Dock'];
   }
-  
+
   boxen::osx_defaults { 'magnification_size':
     domain => 'com.apple.dock',
     key    => 'largesize',
