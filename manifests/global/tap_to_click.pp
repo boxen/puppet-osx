9f5c71b188ee9a72ec4bd3b1b21095a7fe4a60a2
John Colvin <colvin.john@gmail.com>
2014-06-08 18:41:01 -0400

    Fixes lint issues for tap to click class

--- manifests/global/tap_to_click.pp
+++ manifests/global/tap_to_click.pp
@@ -1,19 +1,20 @@
+# Public: Enables Tap to click
 class osx::global::tap_to_click {
   boxen::osx_defaults { 'Tap-To-Click Bluetooth':
     ensure => present,
     domain => 'com.apple.driver.AppleBluetoothMultitouch.trackpad',
-    key => 'Clicking',
-    value => true,
-    type => bool,
-    user => $::boxen_user;
+    key    => 'Clicking',
+    value  => true,
+    type   => bool,
+    user   => $::boxen_user;
   }
 
   boxen::osx_defaults { 'Tap-To-Click Mouse':
     ensure => present,
     domain => 'NSGlobalDomain',
-    key => 'com.apple.mouse.tapBehavior',
-    value => 1,
-    type => int,
-    user => $::boxen_user;
+    key    => 'com.apple.mouse.tapBehavior',
+    value  => 1,
+    type   => int,
+    user   => $::boxen_user;
   }
 }
